9c5c2f34695134220530fd4da9334d0189e45479
wanglin <2321688645@qq.com>
2022-06-28 13:36:09 +0800

    update util.go

--- tools/utils.go
+++ tools/utils.go
@@ -1,20 +1,50 @@
 package tools
 
 import (
+	"bufio"
 	"crypto/ecdsa"
 	"encoding/hex"
 	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	"golang.org/x/crypto/sha3"
 	"golang.org/x/xerrors"
+	"io"
+	"io/ioutil"
+	"os/exec"
 	"strings"
 )
 
 func SignHash(data []byte) []byte {
 	msg := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(data), data)
 	return crypto.Keccak256([]byte(msg))
+}
+
+func GeneratePriKeyHex(no int) []string {
+	priKeys := make([]string, 0)
+	for i := 0; i < no; i++ {
+		priKey, _ := crypto.GenerateKey()
+		hex := common.Bytes2Hex(crypto.FromECDSA(priKey))
+		priKeys = append(priKeys, hex)
+	}
+	return priKeys
+}
+
+func GetPriKey(priPath, pwdPath string) (string, error) {
+	files, err := ioutil.ReadDir(priPath)
+	if err != nil {
+		return "", xerrors.Errorf("read %s fail. %v", priPath, err)
+	}
+	keyJson, _ := ioutil.ReadFile(files[1].Name())
+	passwd, err := ioutil.ReadFile(pwdPath)
+	if err != nil {
+		return "", xerrors.Errorf("read %s fail. %v", pwdPath, err)
+	}
+	key, _ := keystore.DecryptKey(keyJson, strings.Trim(string(passwd), "\n"))
+	privateKey := hexutil.Encode(crypto.FromECDSA(key.PrivateKey))
+	return privateKey, nil
 }
 
 // RecoverAddress recover the address from sig
@@ -63,6 +93,34 @@ func PriKeyToAddress(priKey string) (account common.Address, fromKey *ecdsa.Priv
 	return
 }
 
+func ExecShell(commandPath string) (string, error) {
+	cmd := exec.Command("/bin/sh", "-c", commandPath)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	if err = cmd.Start(); err != nil {
+		return "", err
+	}
+
+	out := bufio.NewReader(stdout)
+	var result string
+	for {
+		line, _, err := out.ReadLine()
+		result += string(line)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return result, err
+		}
+		if strings.Contains(string(line), "--verbosity") {
+			return result, nil
+		}
+	}
+	return result, nil
+}
+
 func ToHex16(src string) string {
 	data := []byte(src)
 	rs := hex.EncodeToString(data)
@@ -73,6 +131,7 @@ func CheckAddress(name, value string) error {
 	if !strings.HasPrefix(value, "0X") && !strings.HasPrefix(value, "0x") {
 		return xerrors.Errorf("%s is not string of 0x", name)
 	}
+
 	if len(value) != 42 {
 		return xerrors.Errorf("the len of %s must be 42", name)
 	}
